8f4a31750f68437b0608cf5993f75f973de3ae5d
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2020-11-29 00:17:22 +0100

    Problem 003 vereinfacht

--- EulerProblemsFSharp.Tests/Problem003Tests.fs
+++ EulerProblemsFSharp.Tests/Problem003Tests.fs
@@ -6,31 +6,16 @@ open NUnit.Framework
 [<SetUp>]
 let Setup () = ()
 
-[<Test>]
-let Test1 () = Assert.Pass()
-
-[<Test>]
-let IsDividedByAnyWorks () =
-    let result = Problem003.isDividedByAny 4I [ 2I; 3I ]
-
-    Assert.AreEqual(true, result)
-
-let primeTestCases =
-    [ [], 2I
-      [ 2I ], 3I
-      [ 3I; 2I ], 5I
-      [ 5I; 3I; 2I ], 7I ]
-    |> List.map (fun (primes, e) -> TestCaseData(primes, e))
-
-[<TestCaseSource("primeTestCases")>]
-let GetPrimesShouldReturnCorrectPrimes primes expected =
-    let result = Problem003.getNextPrime primes
-
-    Assert.AreEqual(expected, result)
 
 [<Test>]
 let getBiggestPrimeFactor2Returns2 () =
-    let result = Problem003.getBiggestPrimeFactor (2I)
+    let result = Problem003.getBiggestPrimeFactor 2I
+
+    Assert.AreEqual(2I, result)
+
+[<Test>]
+let getBiggestPrimeFactor4Returns2 () =
+    let result = Problem003.getBiggestPrimeFactor 4I
 
     Assert.AreEqual(2I, result)
 
--- EulerProblemsFSharp/Problem003.fs
+++ EulerProblemsFSharp/Problem003.fs
@@ -2,45 +2,23 @@ module Problem003
 
 let isDivisibleBy n d = n % d = 0I
 
-let isDividedByAny numerator denominators =
-    denominators
-    |> List.where (numerator |> isDivisibleBy)
-    |> List.isEmpty
-    |> not
-
-
 let firstPrime = 2I
 
-let getNextPrime primes =
-    let rec getNextPrimeRec foundPrimes primeCandidate =
-        match (isDividedByAny primeCandidate foundPrimes) with
-        | false -> primeCandidate
-        | true -> getNextPrimeRec foundPrimes (primeCandidate + 1I)
-
-
-    match primes with
-    | head :: _ -> (getNextPrimeRec primes (head + 1I))
-    | [] -> firstPrime
-
-
 let getPrimeFactors number =
-    let rec getPrimeFactorsRec number primesFullyChecked =
-        let nextPrime = getNextPrime primesFullyChecked
-        match number = 1I with
-        | true -> []
-        | false ->
+    let rec getPrimeFactorsRec number nextFactorToCheck =
+        let nextPrime = nextFactorToCheck
+        match number with
+        | one when one = 1I -> []
+        | _ ->
             match (isDivisibleBy number nextPrime) with
             | true ->
                 nextPrime
-                :: (getPrimeFactorsRec (number / nextPrime) primesFullyChecked)
-            | false -> getPrimeFactorsRec number (nextPrime :: primesFullyChecked)
+                :: (getPrimeFactorsRec (number / nextPrime) nextPrime)
+            | false -> getPrimeFactorsRec number (nextPrime + 1I)
 
-    getPrimeFactorsRec number []
+    getPrimeFactorsRec number firstPrime
 
-let getBiggestPrimeFactor number =
-    let primeFactors = getPrimeFactors number
-    printfn "Prime Factors: %A" primeFactors
-    List.last primeFactors
+let getBiggestPrimeFactor number = getPrimeFactors number |> List.last
 
 let realNumber = 600_851_475_143I
 
